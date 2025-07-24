573b40d3d6651f1122fa5dc1f84dceb2f951af86
Matheus Rosa <matheusnsampaio@gmail.com>
2025-07-24 16:54:37 -0300

    :sparkles: Adding the orchestrator logic

--- orchestrator/orchestrate.py
+++ orchestrator/orchestrate.py
@@ -9,21 +9,60 @@ import httpx
 @task(log_prints = True)
 def scrape_papers(selected_page:str) -> Dict[str, str]:
     try:
-        texts = PaperScraper(page = selected_page).returnable_text()
+        texts = PaperScraper(page = selected_page).create_data()
         return texts
     except Exception as e:
-        return Failed(message = f'Failed to get {selected_page}:{e}')
+        raise Exception(f'Failed to get {selected_page}:{e}')
 
 @task(log_prints = True)
-def post_titles(scraper:List[Dict[str, str]]):
+def transform_titles(titles:List[Dict[str, str]]):
+    try:
+        transformed = []
+        for title in titles:
+            transformed.append(Title(**title))
+        return transformed
+    except Exception as e:
+        raise Exception(f'Failed to transform titles due to:{e}')
+
+@task(log_prints = True)
+def transform_abstracts(abstracts: List[Dict[str, str]]):
+    try:
+        transformed = []
+        for abstract in abstracts:
+            transformed.append(Abstract(**abstract))
+        return transformed
+    except Exception as e:
+        raise Exception(f'Failed to transform abstract due to:{e}')
+
+@task(log_prints = True)
+def post_titles(titles:List[Title]):
     try:
         with httpx.Client() as client:
-            response = client.post()
+            response = client.post('http://localhost:8000/papers/post/titles', data = titles.model_dump())
+        return Completed(message = 'Succesfully posted the titles of each papers')
     except Exception as e:
-        return Failed(message = f'Failed to post titles:{e}')
+        raise Exception(f'Failed to post titles due to:{e}')
+
+@task(log_prints = True)
+def post_abstracts(abstracts: List[Abstract]):
+    try:
+        with httpx.Client() as client:
+            response = client.post('http://localhost:8000/papers/post/abstract', data = abstracts.model_dump())
+        return Completed(message = 'Succesfully posted the abstract of each paper')
+    except Exception as e:
+        raise Exception(f'Failed to post abstracts due to:{e}')
+
 
 @flow(log_prints = True)
 def main_flow():
-    lattest = scrape_papers(selected_page = 'latest').submit()
-    trending = scrape_papers(selected_page = 'trending').submit()
+    latest = scrape_papers(selected_page = 'latest')
+    trending = scrape_papers(selected_page = 'trending')
 
+    titles = latest['titles'] + trending['titles']
+    abstracts = latest['abstracts'] + trending['abstracts']
+    
+    titles = transform_titles(titles = titles)
+    abstracts = transform_abstracts(abstracts = abstracts)
+
+    post_titles(titles = titles)
+    post_abstracts(abstracts = abstracts)
