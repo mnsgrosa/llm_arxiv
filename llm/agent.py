96829a6cd224131ba2a7492ea67c227a06519e44
Matheus Rosa <matheusnsampaio@gmail.com>
2025-07-29 11:29:22 -0300

    :hammer: Refactoring streamlit dockerfile and docker compose

--- llm/agent.py
+++ llm/agent.py
@@ -39,7 +39,7 @@ class MCPTool(BaseTool):
 class ChatPaperAgent:
     def __init__(self):
         self.groq_api_key = os.getenv('GROQ_API_KEY')
-        self.server_url = 'http://localhost:8000/sse/'
+        self.server_url = 'http://llm_arxiv-mcp-server-1:8000/sse/'
         self.mcp_session = None
         self.tools = []
         self.agent_executor = None
@@ -109,7 +109,6 @@ class ChatPaperAgent:
                     
                     self.logger.info("Agent created, invoking...")
                     
-                    # Now invoke while connection is active
                     result = await agent_executor.ainvoke({
                         'input': message,
                         'chat_history': chat_history
